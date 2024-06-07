fa5ff4862bd0cf30ef595f3e6f6540b493fa0c25
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-07 20:46:54 -0300

    Interface for created book

---
+++ src/app/Model/book.ts
@@ -0,0 +1,30 @@
+export interface Book {
+  id?:                  any;
+  title?:               string;
+  author?:              string;
+  yearOfPublication?:   string;
+  active?:              boolean;
+  status?:              Status;
+  bookCondition?:       BookCondition;
+  loans?:               Loan[];
+}
+
+export enum Status {
+  AVAILABLE = "BORROWED",
+  BORROWED = "RETURNED",
+  RESERVED = "OVERDUE"
+}
+
+export enum BookCondition {
+  GOOD = "GOOD",
+  DAMAGED = "DAMAGED",
+  LOST = "LOST"
+}
+
+export interface Loan {
+  id?:           number;
+  loanDate?:     string;
+  returnDate?:   string;
+  bookId?:       number;
+  customerId?:   number;
+}
