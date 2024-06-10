c4e3a26bb4b2e09e08f12f9e5659bed653c304d1
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-10 20:13:46 -0300

    Implemented authentication methods

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Credentials } from '../Model/credentials';
+import { API_CONFIG } from '../Config/api-config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+
+  jwtService: JwtHelperService = new JwtHelperService();
+
+  constructor(private http: HttpClient) { }
+
+  authenticate(creds: Credentials) {
+    return this.http.post(`${API_CONFIG.baseUrl}/login`, creds);
+  }
+
+  successfulLogin(authToken: string, email: string) {
+    localStorage.setItem('token', authToken)
+    localStorage.setItem('email', email)
+  }
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token')
+    if(token != null) {
+      return !this.jwtService.isTokenExpired(token)
+    }
+    return false
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+
 }
