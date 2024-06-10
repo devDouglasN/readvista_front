3a62704462bd225635e1508b13801367fe0e6a5f
Douglas Nascimento <douglas_timaofiel@hotmail.com>
2024-06-10 13:15:02 -0300

    Route to Login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookListComponent } from './components/book/book-list/book-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
 
-  {path: 'nav', component: NavbarComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'book-list', component: BookListComponent},
+  {path: 'login', component: LoginComponent},
 ];
